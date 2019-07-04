07e0044813b91e688ad20a093ca9dcd41fb8888e
Nicolás Pazos <npazosmendez@gmail.com>
2019-07-04 02:12:57 -0300

    Add randonmness to types

--- model/default_datatypes.go
+++ model/default_datatypes.go
@@ -1,5 +1,13 @@
 package model
 
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
 //TODO: Improve this. Doing it as quick as possible
 
 //DataTypeRepository is the central collection of all DataTypes
@@ -25,33 +33,39 @@ func CountStructDataTypes(repository DataTypeRepository) int {
 //common or default DataTypes. From them all others all built.
 func GetDefaultDataTypeRepository() DataTypeRepository {
 	repository := make(DataTypeRepository)
+	randomGenerator := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	//string
 	repository["string"] = &SimpleDataType{
 		name: "string",
 		generator: func() string {
-			return "\"holis\""
+			length := randomGenerator.Intn(20)
+			randString := make([]rune, length)
+			for i := range randString {
+				randString[i] = letters[randomGenerator.Intn(len(letters))]
+			}
+			return `"` + string(randString) + `"`
 		},
 	}
 	//int
 	repository["int"] = &SimpleDataType{
 		name: "int",
 		generator: func() string {
-			return "42"
+			return strconv.Itoa(randomGenerator.Int() % 10000)
 		},
 	}
 	//float64
 	repository["float64"] = &SimpleDataType{
 		name: "float64",
 		generator: func() string {
-			return "66.6"
+			return strconv.FormatFloat(randomGenerator.Float64()*1000, 'f', 3, 32)
 		},
 	}
 	//bool
 	repository["bool"] = &SimpleDataType{
 		name: "bool",
 		generator: func() string {
-			return "true"
+			return []string{"true", "false"}[randomGenerator.Int()%2]
 		},
 	}
 	return repository
