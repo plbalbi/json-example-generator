6cf4adbea92af5a678737202f573e0e80c605d55
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-07-04 23:46:04 -0300

    Typos

--- model/random_generator_test.go
+++ model/random_generator_test.go
@@ -5,14 +5,14 @@ import (
 	"testing"
 )
 
-func TestRandomGeneraterWithSameSeedAllTimes(t *testing.T) {
+func TestRandomGeneratorsWithEqualsSeedGenerateSameNumbers(t *testing.T) {
 	var initialSeed int64
 	initialSeed = 10
 	randomGenerator := rand.NewSource(initialSeed)
-	antoherRandomGenerator := rand.NewSource(initialSeed)
+	anotherRandomGenerator := rand.NewSource(initialSeed)
 
 	for i := 0; i < 10; i++ {
-		if randomGenerator.Int63() != antoherRandomGenerator.Int63() {
+		if randomGenerator.Int63() != anotherRandomGenerator.Int63() {
 			t.Errorf("Both random generator gave a different number")
 		}
 	}
