bc4595c2948e81a04dff978f39646321082cc451
Nicolás Pazos <npazosmendez@gmail.com>
2019-07-05 21:23:41 -0300

    Clean stout

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 
 	"github.com/plbalbi/json-example-generator/parser"
@@ -24,36 +23,30 @@ func main() {
 	var err error
 	switch flag.NArg() {
 	case 0:
-		fmt.Println("Parsing from stdin...")
 		data, err = ioutil.ReadAll(os.Stdin)
 		check(err)
 		break
 	case 1:
-		fmt.Println("Parsing from file...")
 		data, err = ioutil.ReadFile(flag.Arg(0))
 		check(err)
 		break
 	default:
-		fmt.Printf("input must be from stdin or file\n")
+		fmt.Fprintf(os.Stderr, "input must be from stdin or file\n")
 		os.Exit(1)
 	}
 
 	stringToParse := string(data)
-	fmt.Println("String to be parsed:")
-	fmt.Println(stringToParse)
 
 	lexResult, lexError := parser.Parse(stringToParse)
 	if lexError != nil {
-		log.Printf("Errors: %v\n", lexError)
-
+		fmt.Fprintf(os.Stderr, "Parsing failed: %s\n", lexError.Error())
+		os.Exit(1)
 	} else {
-		fmt.Printf("Datatypes counted: %v\n", lexResult.StructsCount())
-		fmt.Printf("First datatype seen: %v\n", lexResult.FirstDataTypeSeen())
 		generatedStruct := lexResult.GenerateDataType()
 		generatedStruct, _ = jsonPrettyPrint(generatedStruct)
-		fmt.Printf("%s", generatedStruct)
+		fmt.Printf("%s\n", generatedStruct)
+		os.Exit(0)
 	}
-
 }
 
 func jsonPrettyPrint(jsonStr string) (string, error) {
