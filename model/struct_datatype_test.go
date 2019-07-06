ca0a352906fe440bc194acd01cefc5e59365a532
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-07-05 22:15:31 -0300

    Fix empty struct testcase

--- model/struct_datatype_test.go
+++ model/struct_datatype_test.go
@@ -3,10 +3,10 @@ package model
 import "testing"
 
 func TestEmptyStructIsWellGenerated(t *testing.T) {
-	expected := "{}"
+	expected := "{\n\n}"
 	emptyStructType := NewStructDataType("emptyStructType")
 	generated := emptyStructType.Generate(nil)
 	if expected != generated {
-		t.Errorf("Failed to generate type random example. Expected: %s, but got: %s", expected, generated)
+		t.Errorf("Failed to generate type random example. Expected:\n%s\n, but got:\n%s", expected, generated)
 	}
 }
